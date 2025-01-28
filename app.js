49150b46b9a6074853e5f913f4118282c40e80e4
Edgar <epgarzonr@uce.edu.ec>
2025-01-27 23:31:11 -0500

    refactor: change server and database setup to English

--- app.js
+++ app.js
@@ -5,14 +5,14 @@ const surveyRoutes = require('./routes/surveyRoutes');
 const sequelize = require('./config/database');
 
 app.use(cors());
-app.use(express.json()); 
+app.use(express.json());
 
 app.use('/api', surveyRoutes);
 
 sequelize.sync({ force: false }).then(() => {
-  console.log("Base de datos sincronizada.");
+  console.log("Database synchronized.");
   const PORT = 3003;
   app.listen(PORT, () => {
-    console.log(`Servidor corriendo en http://localhost:${PORT}`);
+    console.log(`Server running at http://localhost:${PORT}`);
   });
 });
