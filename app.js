dc33f6a073716f68b2490d5a78a4adbf5742cdc5
Edgar <epgarzonr@uce.edu.ec>
2025-01-27 23:30:38 -0500

    feat: configure server with CORS, routes and database synchronization

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const surveyRoutes = require('./routes/surveyRoutes');
+const sequelize = require('./config/database');
+
+app.use(cors());
+app.use(express.json()); 
+
+app.use('/api', surveyRoutes);
+
 sequelize.sync({ force: false }).then(() => {
   console.log("Base de datos sincronizada.");
   const PORT = 3003;
